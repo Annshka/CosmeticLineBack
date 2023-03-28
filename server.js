30d4c0c58759e7bd71ebd5f28f0d301e6acdb9b0
Agnieszka <annies@o2.pl>
2023-03-28 18:57:53 +0200

    Error handling added

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3002;
+const {errorHandler} = require('./utils/errors');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use('/api/packages', require('./routes/packageRoutes'));
 
+
+app.use(errorHandler);
 
 app.listen(port, '127.0.0.1', () => {
     console.log('Listening on port http://localhost:3002')
