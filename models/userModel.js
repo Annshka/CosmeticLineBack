60bc105f6cb642157abcc2e1d3d146b2a5a20da1
Agnieszka <annies@o2.pl>
2023-03-29 10:35:34 +0200

    User Model created

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please enter a username"],
+        min: [3, "Enter min 3 characters"],
+        unique: [true, "Username already exists"]
+    },
+    email: {
+        String,
+        required: [true, "Please enter an email"],
+        unique: [true, "Email address already exists"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+        min: [6, "enter min 6 characters"]
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            },
+        }
+    ]
+},
+    {
+        timestamps: true
+    });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
