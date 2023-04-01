90b0b2832083a1e1eb336d6e359b9b0a9c97ae9c
Agnieszka <annies@o2.pl>
2023-04-01 19:01:37 +0200

    Refactored logic of getting current logged-in user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,13 +81,8 @@ const loginUser = asyncHandler( async (req, res) => {
  // @access Private
  * */
 const getUserId = asyncHandler( async (req, res) => {
-    const {_id, username, email} = await User.findById(req.user.id)
 
-    res.status(200).json({
-        id: _id,
-        username,
-        email
-    })
+    res.status(200).json(req.user)
 });
 
 
